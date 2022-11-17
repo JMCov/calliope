dfe5d81c7af7729e9eeb8a7e534b28a6cc05c974
Jordan Covington <Atlantafan1087@gmail.com>
2022-11-17 15:40:18 -0800

    Added for statements and other loops

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function hello(){
 
     alert("Hello, Welcome to Calliope");
     let usersName = prompt("What is your name?");
+    while (usersName == ""){
+        usersName = prompt("Come on... Give me your name...")
+    }
     console.log("The user's name is ", usersName);
     let penNameOrNot = prompt("Would you like to use a pen name? (yes or no)");
     console.log("The user's response to pen name was: ", penNameOrNot );
@@ -22,3 +25,13 @@ function hello(){
     }
 }
 
+function skillLevel(){
+let skill = prompt("On a scale from 1-10 what would you say your skill at creative writing is?")
+while(skill < 0 || skill > 10 || skill == 0){
+    skill = prompt("Please use a number between 1 and 10")
+}
+for (let i = 0; i <= skill; i++) {
+    document.write("<img class='pencil' src='./images/pencil.jpg' alt='Pencil' width='25' height='50' />")
+}
+
+}
