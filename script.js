819c2ba6fec3a2028c716ec8d49e411da550672f
Jordan Covington <Atlantafan1087@gmail.com>
2022-11-17 16:22:51 -0800

    Resolved for loop

--- script.js
+++ script.js
@@ -27,8 +27,10 @@ function hello(){
 
 function skillLevel(){
 var skill = prompt("On a scale from 1-10 what would you say your skill at creative writing is?")
+
 while(skill < 0 || skill > 10 || skill == 0){
     skill = prompt("Please use a number between 1 and 10")
+   
 }
 for (let i = 0; i < skill; i++) {
     document.write("<img class='pencil' src='./images/pencil.jpg' alt='Pencil' width='25' height='50' />")
